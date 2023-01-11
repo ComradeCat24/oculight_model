b885d074ad5c213f01adfda6969e1fac1566cf2f
comradecat24 <sym@bitfringe.com>
2023-01-11 12:47:39 +0530

    :bug: fixed #1 issue

--- features.py
+++ features.py
@@ -1,44 +1,36 @@
 from os import listdir
-from pickle import dump
-from keras.applications.vgg16 import VGG16, preprocess_input
-from keras.preprocessing.image import load_img, img_to_array
+import pickle 
+from keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input
+from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Model
 
 # Preparing Photo Data
 
-# The code first loads the VGG16 model and removes the last layers so that it
-# only outputs the features of the last fully connected layer. Then, it reads
-# all image files in the specified directory, loads them, resizes them to (224,
-# 224) pixels, preprocesses them, and uses the VGG16 model to extract features
-# from each image.
-
-# It then associates the image id with the extracted features from the image and
-# store in features dictionary.
-
 def extract_features(directory):
-	model = VGG16()
+
+	model = MobileNetV2(weights='imagenet', include_top=False)
 	model = Model(inputs=model.inputs, outputs=model.layers[-2].output)
 	print(model.summary())
+
 	features = {}
 
-	for name in listdir(directory):
-		filename = directory + '/' + name
-		# print(filename)
-		image = load_img(filename, target_size=(224, 224))
-		image = img_to_array(image)
-		image = image.reshape(
-			(1, image.shape[0], image.shape[1], image.shape[2])
-		)
-		image = preprocess_input(image)
-		feature = model.predict(image, verbose=0)
-		image_id = name.split('.')[0]
-		features[image_id] = feature
-		print('>%s' % name)
+	datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
+
+	# Generate batches of image data from the specified directory
+	data_gen = datagen.flow_from_directory(directory, target_size=(150, 150), batch_size=32, class_mode=None)
+
+	# Extract features from each batch
+	for features_batch in model.predict(data_gen, verbose=1):
+		for i in range(len(data_gen.filenames)):
+			image_id = data_gen.filenames[i].split('.')[0]
+			features[image_id] = features_batch[i]
+			print('>%s' % data_gen.filenames[i])
+
+	with open('features.pickle', 'wb') as handle:
+		pickle.dump(features, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 	return features
 
-
 directory = 'Flicker30k_Dataset'
 features = extract_features(directory)
-print('Extracted Features: %d' % len(features))
-dump(features, open('features.pkl', 'wb'))
+
