293a39bdc348d18ec84afb4a9c5c8094521203b2
comradecat24 <sym@bitfringe.com>
2023-01-27 13:52:47 +0530

    :construction: clean text.py file

--- text.py
+++ text.py
@@ -1,68 +1,85 @@
 import string
 
-# Prepare Text Data
 
 def load_doc(filename):
-	file = open(filename, 'r')
-	text = file.read()
-	file.close()
-	return text
+    """
+    Loads a text file and returns its content as a string.
+    """
+    with open(filename, 'r') as file:
+        text = file.read()
+    return text
 
 
 def load_descriptions(doc):
-	mapping = dict()
-	for line in doc.split('\n'):
-		tokens = line.split()
-		if len(line) < 2:
-			continue
-		image_id, image_desc = tokens[0], tokens[1:]
-		image_id = image_id.split('.')[0]
-		image_desc = ' '.join(image_desc)
-		if image_id not in mapping:
-			mapping[image_id] = list()
-		mapping[image_id].append(image_desc)
-	return mapping
+    """
+    Loads a document and returns a mapping of image IDs to their corresponding
+    descriptions.
+    """
+    mapping = dict()
+    for line in doc.split('\n'):
+        tokens = line.split()
+        if len(line) < 2:
+            continue
+        image_id, image_desc = tokens[0], tokens[1:]
+        image_id = image_id.split('.')[0]
+        image_desc = ' '.join(image_desc)
+        if image_id not in mapping:
+            mapping[image_id] = list()
+        mapping[image_id].append(image_desc)
+    return mapping
 
 
 def clean_descriptions(descriptions):
-	table = str.maketrans('', '', string.punctuation)
-	for key, desc_list in descriptions.items():
-		for i in range(len(desc_list)):
-			desc = desc_list[i]
-			desc = desc.split()
-			desc = [word.lower() for word in desc]
-			desc = [w.translate(table) for w in desc]
-			desc = [word for word in desc if len(word) > 1]
-			desc = [word for word in desc if word.isalpha()]
-			desc_list[i] = ' '.join(desc)
+    """
+    Cleans the descriptions by removing punctuation, lowercasing all words, and
+    removing words that are not alphabetic or have a length less than 2.
+    """
+    table = str.maketrans('', '', string.punctuation)
+    for key, desc_list in descriptions.items():
+        for i in range(len(desc_list)):
+            desc = desc_list[i]
+            desc = desc.split()
+            desc = [word.lower() for word in desc]
+            desc = [w.translate(table) for w in desc]
+            desc = [word for word in desc if len(word) > 1]
+            desc = [word for word in desc if word.isalpha()]
+            desc_list[i] = ' '.join(desc)
 
 
 def to_vocabulary(descriptions):
-	all_desc = set()
-	for key in descriptions.keys():
-		[all_desc.update(d.split()) for d in descriptions[key]]
-	return all_desc
+    """
+    Extracts a set of all unique words from the descriptions.
+    """
+    all_desc = set()
+    for key in descriptions.keys():
+        [all_desc.update(d.split()) for d in descriptions[key]]
+    return all_desc
 
 
 def save_descriptions(descriptions, filename):
-	lines = list()
-	for key, desc_list in descriptions.items():
-		for desc in desc_list:
-			lines.append(key + ' ' + desc)
-	data = '\n'.join(lines)
-	file = open(filename, 'w')
-	file.write(data)
-	file.close()
+    """
+    Saves the descriptions to a text file.
+    """
+    lines = list()
+    for key, desc_list in descriptions.items():
+        for desc in desc_list:
+            lines.append(key + ' ' + desc)
+    data = '\n'.join(lines)
+    with open(filename, 'w') as file:
+        file.write(data)
 
 
-filename = 'Flickr30k.token.txt'
-doc = load_doc(filename)
-descriptions = load_descriptions(doc)
-print(len(descriptions))
-# print(descriptions)
-clean_descriptions(descriptions)
-vocabulary = to_vocabulary(descriptions)
-print(len(vocabulary))
-# print(vocabulary)
+if __name__ == '__main__':
+    filename = 'dataset/captions.txt'
+    doc = load_doc(filename)
+    descriptions = load_descriptions(doc)
+    # print(descriptions)
+    print(len(descriptions))
+    clean_descriptions(descriptions)
+    vocabulary = to_vocabulary(descriptions)
+    # print(vocabulary)
+    print(len(vocabulary))
+    save_descriptions(descriptions, 'descriptions.txt')
+
 
 save_descriptions(descriptions, 'descriptions.txt')
