862befcbafebffc35de8921bca21f49d2a783447
comradecat24 <sym@bitfringe.com>
2023-02-12 13:41:41 +0530

    :ambulance: cleaning data rewrite - added comment_number in desc.txt file - improved extraction of vocab

--- text.py
+++ text.py
@@ -8,7 +8,8 @@ def load_doc(filename):
     try:
         with open(filename, 'r', encoding='utf-8-sig') as file:
             text = file.read()
-    except:
+    except IOError as e:
+        print(e)
         print("Error occured while reading the file")
         return None
     return text
@@ -21,15 +22,16 @@ def load_descriptions(doc):
     """
     mapping = dict()
     for line in doc.split('\n'):
-        tokens = line.split()
-        if len(line) < 2:
+        tokens = line.split(',')
+        if len(tokens) < 3:
             continue
-        image_id, image_desc = tokens[0], tokens[1:]
+        image_id, image_desc_id, *image_desc = tokens
         image_id = image_id.split('.')[0]
-        image_desc = ' '.join(image_desc)
+        image_desc_id = image_desc_id.strip()
+        image_desc = ', '.join(image_desc).strip()
         if image_id not in mapping:
-            mapping[image_id] = list()
-        mapping[image_id].append(image_desc)
+            mapping[image_id] = []
+        mapping[image_id].append((image_desc_id, image_desc))
     return mapping
 
 
@@ -39,15 +41,15 @@ def clean_descriptions(descriptions):
     removing words that are not alphabetic or have a length less than 2.
     """
     table = str.maketrans('', '', string.punctuation)
-    for key, desc_list in descriptions.items():
+    for image_name, desc_list in descriptions.items():
         for i in range(len(desc_list)):
-            desc = desc_list[i]
+            desc_id, desc = desc_list[i][0], desc_list[i][1]
             desc = desc.split()
             desc = [word.lower() for word in desc]
             desc = [w.translate(table) for w in desc]
             desc = [word for word in desc if len(word) > 1]
             desc = [word for word in desc if word.isalpha()]
-            desc_list[i] = ' '.join(desc)
+            desc_list[i] = (desc_id, ' '.join(desc))
 
 
 def to_vocabulary(descriptions):
@@ -55,32 +57,33 @@ def to_vocabulary(descriptions):
     Extracts a set of all unique words from the descriptions.
     """
     all_desc = set()
-    for key in descriptions.keys():
-        [all_desc.update(d.split()) for d in descriptions[key]]
+    for img_id in descriptions:
+        for desc in descriptions[img_id]:
+            all_desc.update(desc[1].split())
     return all_desc
 
 
 def save_descriptions(descriptions, filename):
     """
-    Saves the descriptions to a text file.
+    Saves the descriptions to a text file in the format "image_name, comment_number, comment".
     """
     lines = list()
-    for key, desc_list in descriptions.items():
-        for desc in desc_list:
-            lines.append(key + ' ' + desc)
+    for img_id in descriptions.keys():
+        img_descs = descriptions[img_id]
+        for desc in img_descs:
+            lines.append(img_id + ', ' + desc[0] + ', ' + desc[1])
     data = '\n'.join(lines)
     with open(filename, 'w', encoding='utf-8') as file:
         file.write(data)
 
 
-if __name__ == '__main__':
-    filename = 'dataset/captions.txt'
-    doc = load_doc(filename)
-    descriptions = load_descriptions(doc)
-    # print(descriptions)
-    print(len(descriptions))
-    clean_descriptions(descriptions)
-    vocabulary = to_vocabulary(descriptions)
-    # print(vocabulary)
-    print(len(vocabulary))
-    save_descriptions(descriptions, 'descriptions.txt')
+filename = 'subset_dataset/subset_captions.txt'
+doc = load_doc(filename)
+descriptions = load_descriptions(doc)
+# print(descriptions)
+print(len(descriptions))
+clean_descriptions(descriptions)
+vocabulary = to_vocabulary(descriptions)
+# print(vocabulary)
+print(len(vocabulary))
+save_descriptions(descriptions, 'descriptions.txt')
