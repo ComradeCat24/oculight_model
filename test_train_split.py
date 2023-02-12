e878cb653aaecef39bf16a884533f4a5682fde17
comradecat24 <sym@bitfringe.com>
2023-02-12 15:59:35 +0530

    :truck: test_train_split - splitting dataset into test, train and validation sets

--- test_train_split.py
+++ test_train_split.py
@@ -1,35 +1,44 @@
 import os
 import pandas as pd
-from tqdm import tqdm
 from sklearn.model_selection import train_test_split
 
 
 def split_data(caption_file, test_size=0.2, validation_size=0.2):
     if not os.path.isfile(caption_file):
         raise FileNotFoundError(f"{caption_file} not found.")
-    # Extracting the base file name
+
+    # Extract the base file name
     base_file_name = os.path.splitext(os.path.basename(caption_file))[0]
 
-    # Generating file names
-    train_file = f"dataset/splits/{base_file_name}.train.txt"
-    validation_file = f"dataset/splits/{base_file_name}.validation.txt"
-    test_file = f"dataset/splits/{base_file_name}.test.txt"
+    # Create a new directory to store the selected images and captions
+    subset_dir = os.path.join(os.getcwd(), "subset_dataset")
+    splits_dir = os.path.join(subset_dir, "splits")
+    os.makedirs(splits_dir, exist_ok=True)
 
-    data = pd.read_csv(caption_file, delimiter='\t')
+    # Generate file names
+    train_file = os.path.join(splits_dir, f"{base_file_name}.train.txt")
+    validation_file = os.path.join(
+        splits_dir, f"{base_file_name}.validation.txt")
+    test_file = os.path.join(splits_dir, f"{base_file_name}.test.txt")
 
-    # Splitting the data into train, validation and test sets
+    # Load data
+    data = pd.read_csv('descriptions.txt', delimiter=',', skiprows=[0], header=None, names=[
+                       'image_name', 'comment_number', 'comment'])
+    data = data.drop_duplicates(subset=["image_name"], keep='first')
+    print(data)
+
+    # Split data into train, validation, and test sets
     train_data, test_data = train_test_split(
         data, test_size=test_size, random_state=42)
     train_data, validation_data = train_test_split(
         train_data, test_size=validation_size, random_state=42)
 
-    # Saving the data in separate files
-    for filename, data in tqdm([(train_file, train_data), (validation_file, validation_data), (test_file, test_data)], desc="Saving files"):
+    # Save data in separate files
+    for filename, data in [(train_file, train_data), (validation_file, validation_data), (test_file, test_data)]:
         with open(filename, 'w') as file:
             for _, row in data.iterrows():
-                file.write(
-                    row['image_name,comment_number,comment'].split(',')[0])
+                file.write(row['image_name'])
                 file.write('\n')
 
 
-split_data('dataset/captions.txt')
+split_data('subset_dataset/subset_captions.txt')
--- text.py
+++ text.py
@@ -26,7 +26,8 @@ def load_descriptions(doc):
         if len(tokens) < 3:
             continue
         image_id, image_desc_id, *image_desc = tokens
-        image_id = image_id.split('.')[0]
+        # image_id = image_id.split('.')[0]
+        image_id = image_id
         image_desc_id = image_desc_id.strip()
         image_desc = ', '.join(image_desc).strip()
         if image_id not in mapping:
