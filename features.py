a23e8154d685094ac85ca83278129deaa6ea9355
comradecat24 <sym@bitfringe.com>
2023-03-30 15:53:09 +0530

    :lock: preparing env variables

--- features.py
+++ features.py
@@ -1,12 +1,16 @@
-from collections import OrderedDict
-from os import listdir
-from pickle import dump
-from keras.applications.vgg16 import VGG16, preprocess_input
+# fmt: off
+import os
 import numpy as np
-# from keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input
-from keras.preprocessing.image import ImageDataGenerator
-from keras.utils import load_img, img_to_array
+from pickle import dump
+from dotenv import load_dotenv
+load_dotenv()
 from keras.models import Model
+from collections import OrderedDict
+from keras.utils import load_img, img_to_array
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications.vgg16 import VGG16, preprocess_input
+# from keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input
+# fmt: on
 
 
 def extract_features(directory):
@@ -21,7 +25,7 @@ def extract_features(directory):
     datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
 
     def feature_generator(directory):
-        for name in listdir(directory):
+        for name in os.listdir(directory):
             filename = directory + '/' + name
             image_id = name.split(".")[0]
             image = load_img(filename, target_size=(224, 224))
@@ -39,8 +43,10 @@ def extract_features(directory):
     return features
 
 
-directory = 'subset_dataset/selected_images'
+directory = os.environ.get('IMAGE_DIRECTORY_PATH')
 features = extract_features(directory)
 print(f'Features: ${features}')
 print('Extracted Features: %d' % len(features))
-dump(features, open('features.pkl', 'wb'))
+
+pickle_file = os.environ.get('FEATURE_PICKLE_FILE')
+dump(features, open(pickle_file, 'wb'))
