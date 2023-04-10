25776b07006e2cde30b2d430605bfda3ca489abf
comradecat24 <sym@bitfringe.com>
2023-04-10 12:49:53 +0530

    :ambulance: - dumping features as soon as they get generated

--- extract_image_features.py
+++ extract_image_features.py
@@ -1,7 +1,6 @@
 # fmt: off
 import os
-import numpy as np
-from pickle import dump
+import pickle 
 from dotenv import load_dotenv
 load_dotenv()
 from keras.models import Model
@@ -16,37 +15,44 @@ from keras.applications.vgg16 import VGG16, preprocess_input
 def extract_features(directory):
 
     model = VGG16(include_top=False, input_shape=(224, 224, 3))
-    # OR
-    # model = MobileNetV2(weights='imagenet', include_top=False)
-
     model = Model(inputs=model.inputs, outputs=model.layers[-2].output)
     model.summary()
 
     datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
 
-    def feature_generator(directory):
-        for name in os.listdir(directory):
-            filename = directory + '/' + name
-            image_id = name.split(".")[0]
-            image = load_img(filename, target_size=(224, 224))
-            image = img_to_array(image)
-            image = image.reshape(
-                (1, image.shape[0], image.shape[1], image.shape[2]))
-            feature = model.predict(datagen.flow(
-                image, batch_size=32)).flatten()
-            print(np.array(feature).shape)
+    # Load existing features from pickle file
+    pickle_file = os.environ.get('IMAGE_FEATURES_FILE')
+    if os.path.exists(pickle_file):
+        with open(pickle_file, 'rb') as f:
+            features = pickle.load(f)
+    else:
+        features = OrderedDict()
 
-            print('>%s' % name)
-            yield image_id, feature
+    for name in os.listdir(directory):
+        filename = directory + '/' + name
+        image_id = name.split(".")[0]
 
-    features = OrderedDict(feature_generator(directory))
-    return features
+        # Check if image_id is already present in the features OrderedDict
+        if image_id in features:
+            print(
+                f'Skipping image {name} as its features are already extracted.')
+            continue
+
+        image = load_img(filename, target_size=(224, 224))
+        image = img_to_array(image)
+        image = image.reshape(
+            (1, image.shape[0], image.shape[1], image.shape[2]))
+        feature = model.predict(datagen.flow(image, batch_size=32)).flatten()
+
+        print('>%s' % name)
+        features.update({image_id: feature})
+
+        # Save updated features to pickle file
+        with open(pickle_file, 'wb') as f:
+            pickle.dump(features, f)
+
+        print('Extracted Features: %d' % len(features))
 
 
 directory = os.environ.get('IMAGE_DIRECTORY_PATH')
-features = extract_features(directory)
-print(f'Features: ${features}')
-print('Extracted Features: %d' % len(features))
-
-pickle_file = os.environ.get('IMAGE_FEATURES_FILE')
-dump(features, open(pickle_file, 'wb'))
+extract_features(directory)
