106fd149b935a55ef1492a62c25a515f29835dc7
comradecat24 <sym@bitfringe.com>
2023-04-23 15:45:36 +0530

    subset data

--- text_processing_f8k.py
+++ text_processing_f8k.py
@@ -1,7 +1,8 @@
-with open('subset_data/captions.txt', 'r') as f_in, open('subset_data/captions_with_id.txt', 'w') as f_out:
+with open('dataset_flickr8k/captions.txt', 'r') as f_in, open('dataset_flickr8k/captions_with_id.txt', 'w') as f_out:
     counter = 0
     for line in f_in:
-        filename, caption = line.strip().split(',', 1)
+        filename, caption_with_tab = line.strip().split('#', 1)
+        caption = caption_with_tab.split('\t')[1]
         caption_id = counter % 5
         f_out.write(f"{filename},{caption_id},{caption}\n")
         counter += 1
